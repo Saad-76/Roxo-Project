9d6aed97347256ec43e9be47ccd2538ebaeb3a0b
hafizahmed420 <hafizahmed420@gmail.com>
2022-01-14 12:12:27 +0500

    Dash header

--- src/Dashboard/dashboardHeader.js
+++ src/Dashboard/dashboardHeader.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import Logo from "../Assests/25.png"
+import {FiSettings} from "react-icons/fi"
+import "./dashboardHeader.css";
 const  DashboardHeader=()=> {
     return (
-        <div>
+        
+       <div className="header-login col-sm-12">
+      <div className="header-login-logo"><img src={Logo} height="70px"></img>
+            <h5>ROXO</h5></div>
+      <div className='header-dash-setting'>< FiSettings size={35}/>
+      <h6>User Name</h6>
+      <button >Log out</button></div>
+    </div>
        
-        </div>
     )
 }
 

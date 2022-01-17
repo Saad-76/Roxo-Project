1f030d681aff9435547596231e9e0b29bf019e75
saad-68 <95625780+Saad-76@users.noreply.github.com>
2022-01-16 23:36:15 -0800

    Modal Done

--- src/NFT-Dashboard/depositeModal.js
+++ src/NFT-Dashboard/depositeModal.js
@@ -15,9 +15,9 @@ const DepositeModal=()=> {
       <div class="modal-dialog">
         <div class="modal-content">
          
-          <div class="modal-body">
-        <h6>Deposite:</h6>
-        <div >
+          <div class="modal-style">
+        <h6 className="depositeModal-heading">Deposite:</h6>
+        <div className="depositModal-makeSure" >
             <h6>Make Sure:</h6>
             <p>1. Send only ROXO to this deposit address.</p>
             <p>2. Ensure that the network is same while sending.</p>
@@ -25,17 +25,17 @@ const DepositeModal=()=> {
 
         <div className="deposit-address">
             <h6>Address: </h6>
-            <p>0x00000000000000000000000000000000000</p>
+            <p className="address-field" >0x00000000000000000000000000000000000</p>
         </div>
 
         <div className="deposit-network">
             <h6>Select Network: </h6>
-            <p>Ensure the network is same while sending.</p>
+            <p className="network-field">Ensure the network is same while sending.</p>
         </div>
 
         <div className="deposit-minimum">
             <h6>Minimum Deposit: </h6>
-            <p>0.2342 ROXO</p>
+            <p className="minimumDeposite-field">0.2342 ROXO</p>
         </div>
 
           </div>
--- src/NFT-Dashboard/withDrawModal.js
+++ src/NFT-Dashboard/withDrawModal.js
@@ -15,11 +15,31 @@ const WithDrawModal=()=> {
       <div class="modal-dialog">
         <div class="modal-content">
          
-          <div class="modal-body">
-            <div class=" col-md-12 ">
-         
-         
+        <div class="withDraw-modal-style">
+        <h6 className="withDrawModal-heading">WithDraw:</h6>
+        
+
+        <div className="withDraw-address">
+            <h6>WithDraw Address: </h6>
+            <p className="withdraw-address-field" >0x00000000000000000000000000000000000</p>
         </div>
+
+        <div className="withDraw-network">
+            <h6>Select Network: </h6>
+            <p className="withDraw-network-field">Ensure the network is same while sending.</p>
+        </div>
+
+        <div className="withDraw-Amount">
+            <h6>WithDraw Amount: </h6>
+            <p className="withDraw-Amount-field">0.2342 ROXO</p>
+        </div>
+        <div className="withDraw-Receive">
+            <h6>Receive Amount: </h6>
+            <p className="withDraw-recieve-field">7990 ROXO<br/>ROXO network fee included</p>
+        </div>
+<div className="withDrawModal-button">
+<button className="withDrawModal-button-style"> WithDraw</button>
+</div>
           </div>
        
       

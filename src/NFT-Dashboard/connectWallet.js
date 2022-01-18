c8446a3eb52297ee95d0a3ac994a038ca68e945c
saad-68 <95625780+Saad-76@users.noreply.github.com>
2022-01-17 19:45:57 -0800

    Push

--- src/NFT-Dashboard/connectWallet.js
+++ src/NFT-Dashboard/connectWallet.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./connectWallet.css";
+import GAME from "../Assests/GAME.jpg"
+
 
 const ConnectWallet = () => {
   return (
@@ -7,6 +9,11 @@ const ConnectWallet = () => {
       <h2 className="connect-heading">Dashboard</h2>
       <div className="col-md-12 connect-main-section">
 <div className="col-md-4">
+<div className="connect-GameName">
+<img src={GAME} alt="game" width="40px" height="40px"/>
+    <p className="connect-gameName-heading">Game Name</p>
+    </div>
+
 
 </div>
 
--- src/NFT-Dashboard/tokenBalance.js
+++ src/NFT-Dashboard/tokenBalance.js
@@ -1,140 +1,147 @@
-import React from 'react'
-import "./tokenBalance.css"
-import RoxoCoin from "../Assests/3.png"
-import GAME from "../Assests/GAME.jpg"
-import FINANCE from "../Assests/FINANCE.jpg"
-import NFT from "../Assests/NFT.jpg"
+import React from "react";
+import "./tokenBalance.css";
+import RoxoCoin from "../Assests/3.png";
+import NftDashboard from "../Assests/NftDashboard.png";
+import Dashboard from "../Assests/Dashboard.png";
 
-
-const TokenBalance=()=> {
-    return (
-        <>
-         <div className="col-md-12 balance-style"> 
-<div className="col-md-4">
-    <div className="balance-GameName">
-<img src={GAME} alt="game" width="40px" height="40px"/>
-    <p className="gameName-heading">Game Name</p>
-    </div>
-    <div className="balance-dashboard">
-    <img src={FINANCE} alt="game" width="40px" height="40px"/>
-    <p className="dashboard-heading">Dashboard</p>
-</div>
-<div className="nft-dashboard">
-    <img src={NFT} alt="game" width="40px" height="40px"/>
-    <p className="nft-heading">NFTs</p>
-    </div>
-
-</div>
-
-<div className="col-md-4  ">
-
-
-<div className=" balance-firstCard-style">
-<div className="balance-firstCard-background">
-<div className="firstCard-backColor">
-<div className="balance-firstCard-inner">
-    <img src={RoxoCoin} alt="roxo" width="100px" height="100px"/>
-    <p>Token Balance <br/>17832.12 ROXO</p>
-    </div>
-    <p>Token value in</p>
-    <div className="token-value">
-        <p>3223<br/>USD</p>
-        <p>0.731<br/>ETH</p>
-        <p>0.12231<br/>BTC</p>
-    </div>
-</div>
-</div>
-</div>
-<div className="firstCard-button">
-<button className="firstCard-button-style">Buy More</button>
-<button className="firstCard-button-style"  data-bs-toggle="modal"
-            data-bs-target="#DepositeModal" >Deposit</button>
-<button className="firstCard-button-style" data-bs-toggle="modal"
-            data-bs-target="#WithDrawModal">With Draw</button>
-</div>
-
-
-{/* ------------------Third-Cardd---------------- */}
-<div className="thirdCard-main">
-  <div className="third-card-set">  
-<div className="thirdCard-backColor" >
-<h6>NFTs balance</h6>
-<div className="balance-thirdCard-data">
-    <p>Total NFTs OWN:</p> 
-    <p>300</p>
-</div>
-<div className="balance-thirdCard-data">
-    <p>Total value:</p> 
-    <p>2.31 ETH <br/> ($8733.12)</p>
-</div>
-</div>
-
-</div>
-
-</div>
-<div className="thirdCard-button">
-<button className="thirdCard-button-style">With Draw</button>
-</div>
-
-
-
-
-</div>
-
-
-<div className="col-md-4">
-<div className="balance-secondCard-background">
-    <div className="secondCard-backColor">
-<div className="hasteKing-button" >
-    <button>Haste King </button>
-
-    </div>
-<div className="balance-secondCard-inner">
-<img src={RoxoCoin} alt="roxo" width="100px" height="100px"/>
-    <p>Token Coins <br/>17832.12 ROXO</p>
-    <button className="convert-button-secondCard" >Convert</button>
-    </div>
-    <div className="global-rank">
-        <p>Global Rank</p>
-        <p>3</p>
-    </div>
-    <div className="balance-stats-button">
-    <button > Stats</button>
-    </div>
-    <div className="stat-fields">
-        <p>Total Score:</p>
-        <p>27482</p>
+const TokenBalance = () => {
+  return (
+    <>
+      <div className="col-md-12 balance-style">
+        <div className="col-md-4">
+            <div>
+          <div className="balance-dashboard">
+            
+            <img src={Dashboard} alt="game" width="40px" height="40px" />
+            <p className="dashboard-heading">Dashboard</p>
+       
+          </div>
+          </div>
+          <div className="nft-dashboard">
+            <img src={NftDashboard} alt="game" width="40px" height="40px" />
+            <p className="nft-heading">NFTs</p>
+          </div>
         </div>
 
-        <div className="stat-fields">
-        <p>Total Races:</p>
-        <p>1322</p>
+        <div className="col-md-4  ">
+          <div className=" balance-firstCard-style">
+            <div className="balance-firstCard-background">
+              <div className="firstCard-backColor">
+                <div className="balance-firstCard-inner">
+                  <img src={RoxoCoin} alt="roxo" width="100px" height="100px" />
+                  <p>
+                    Token Balance <br />
+                    17832.12 ROXO
+                  </p>
+                </div>
+                <p>Token value in</p>
+                <div className="token-value">
+                  <p>
+                    3223
+                    <br />
+                    USD
+                  </p>
+                  <p>
+                    0.731
+                    <br />
+                    ETH
+                  </p>
+                  <p>
+                    0.12231
+                    <br />
+                    BTC
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="firstCard-button">
+            <button className="firstCard-button-style">Buy More</button>
+            <button
+              className="firstCard-button-style"
+              data-bs-toggle="modal"
+              data-bs-target="#DepositeModal"
+            >
+              Deposit
+            </button>
+            <button
+              className="firstCard-button-style"
+              data-bs-toggle="modal"
+              data-bs-target="#WithDrawModal"
+            >
+              With Draw
+            </button>
+          </div>
+
+          {/* ------------------Third-Cardd---------------- */}
+          <div className="thirdCard-main">
+            <div className="third-card-set">
+              <div className="thirdCard-backColor">
+                <h6>NFTs balance</h6>
+                <div className="balance-thirdCard-data">
+                  <p>Total NFTs OWN:</p>
+                  <p>300</p>
+                </div>
+                <div className="balance-thirdCard-data">
+                  <p>Total value:</p>
+                  <p>
+                    2.31 ETH <br /> ($8733.12)
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="thirdCard-button">
+            <button className="thirdCard-button-style">With Draw</button>
+          </div>
         </div>
 
-        <div className="stat-fields">
-        <p>Race Wons:</p>
-        <p>1933</p>
+        <div className="col-md-4">
+          <div className="balance-secondCard-background">
+            <div className="secondCard-backColor">
+              <div className="hasteKing-button">
+                <button>Haste King </button>
+              </div>
+              <div className="balance-secondCard-inner">
+                <img src={RoxoCoin} alt="roxo" width="100px" height="100px" />
+                <p>
+                  Token Coins <br />
+                  17832.12 ROXO
+                </p>
+                <button className="convert-button-secondCard">Convert</button>
+              </div>
+              <div className="global-rank">
+                <p>Global Rank</p>
+                <p>3</p>
+              </div>
+              <div className="balance-stats-button">
+                <button> Stats</button>
+              </div>
+              <div className="stat-fields">
+                <p>Total Score:</p>
+                <p>27482</p>
+              </div>
+
+              <div className="stat-fields">
+                <p>Total Races:</p>
+                <p>1322</p>
+              </div>
+
+              <div className="stat-fields">
+                <p>Race Wons:</p>
+                <p>1933</p>
+              </div>
+
+              <div className="stat-fields">
+                <p>Race Loses:</p>
+                <p>339</p>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <div className="stat-fields">
-        <p>Race Loses:</p>
-        <p>339</p>
-        </div>
-
-</div>
-</div>
-
-
-</div>
-
-
-
-         </div>
-
-
-
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default TokenBalance;
